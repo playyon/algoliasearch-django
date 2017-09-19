f7f1096df611993905073cd6a67ff5e438f418d8
Allan Mercado <mapes911@gmail.com>
2017-09-19 16:36:49 -0700

    check objectID for save_record too

--- algoliasearch_django/models.py
+++ algoliasearch_django/models.py
@@ -290,12 +290,12 @@ class AlgoliaIndex(object):
             if update_fields:
                 obj = self.get_raw_record(instance,
                                           update_fields=update_fields)
-                if not objectID:
+                if 'objectID' in obj and not obj['objectID']:
                     return
                 self.__index.partial_update_object(obj)
             else:
                 obj = self.get_raw_record(instance)
-                if not objectID:
+                if 'objectID' in obj and not obj['objectID']:
                     return
                 self.__index.save_object(obj)
             logger.info('SAVE %s FROM %s', obj['objectID'], self.model)
